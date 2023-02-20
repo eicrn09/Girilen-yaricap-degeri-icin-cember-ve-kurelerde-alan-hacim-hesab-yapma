0f0f58981629f02b5583e59e444542781e08e786
Toshiba <Toshiba@DESKTOP-B345MNB>
2023-02-20 18:28:29 +0300

    Proje dosyası ekle.

---
+++ süüüüüü/süüüüüü.py
@@ -0,0 +1,17 @@
+﻿r=float(input('Yarı Çap:'))
+pi=3.14159
+cevre=r*2*pi
+alan= pi*(r**2)
+print("Çember İçin:")
+print("çevre:", cevre)
+print("alan:", alan)
+
+
+print("Küre İçin:")
+print("Kesit Alanı:", alan)
+yuzeyAlani=4*pi*(r**2)
+print("Yüzey Alanı:", yuzeyAlani)
+hacim=(4/3)*pi*(r**3)
+print("hacim:",hacim)
+dayaniklilik= alan/hacim
+print("Dayanıklılık:",dayaniklilik)
